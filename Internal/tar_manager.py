f50a342fbcaa09e242a7c87f65f22f1f145634bf
nxi <nxi@ansto.gov.au>
2021-11-09 09:57:28 +1100

    update tar manager

--- Internal/tar_manager.py
+++ Internal/tar_manager.py
@@ -87,7 +87,8 @@ def auto_fill():
 
     # Sort list of files based on last modification time in ascending order
     list_of_files = sorted( list_of_files,
-                            key = os.path.getmtime)
+                            key = os.path.getmtime, 
+                            reverse = True)
     # Iterate over sorted list of files and print file path 
     # along with last modification time of file 
     for file_path in list_of_files:
