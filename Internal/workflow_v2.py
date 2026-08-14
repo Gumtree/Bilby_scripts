b976260f9126731bd69883edabcb52af0c0bf266
Norman Xiong <norman.xiong@ansto.gov.au>
2026-08-14 15:31:45 +1000

    use beam monitor

--- Internal/workflow_v2.py
+++ Internal/workflow_v2.py
@@ -2264,15 +2264,16 @@ def run_scan():
     global _start_timestamp
     is_ready = False
     bm_msg = ''
+#    try:
+#        is_ready = control.get_value('/instrument/sis/status/ready') == 'TRUE'
+#    except: 
+#        pass
     try:
-        is_ready = control.get_value('/instrument/sis/status/ready') == 'TRUE'
-    except: 
-        try:
-            slog('test instrument ready with beam monitor server ...')
-            (is_ready,bm_msg) = is_beam_open()
-        except Exception as e:
-            is_ready = False
-            bm_msg = 'Failed to communicate with beam monitor server. ' + str(e)
+        slog('test instrument ready with beam monitor server ...')
+        (is_ready,bm_msg) = is_beam_open()
+    except Exception as e:
+        is_ready = False
+        bm_msg = 'Failed to communicate with beam monitor server. ' + str(e)
     if not is_ready:
         is_confirmed = open_question('The instrument is not ready '\
                     + 'according to the SIS status. ' + bm_msg + 'Please get the '\
